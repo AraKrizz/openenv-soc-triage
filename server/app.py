c3c3838182f4478c3afa0d608ca383b6214cf9c8
Aravind Krishnan <116485715+AraKrizz@users.noreply.github.com>
2026-04-06 03:18:04 +0530

    Add files via upload

---
+++ server/app.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+import uvicorn
+from env import SOCTriageEnv
+from models import Action
+
+app = FastAPI()
+my_env = SOCTriageEnv()
+
+@app.get("/")
+def ping():
+    return {"status": "OK", "message": "CyberGuard SOC Triage is running"}
+
+@app.post("/reset")
+def reset_env():
+    return my_env.reset()
+
+@app.post("/step")
+def step_env(action: Action):
+    obs, reward, done, info = my_env.step(action)
+    return {"observation": obs, "reward": reward, "done": done, "info": info}
+
+@app.get("/state")
+def state_env():
+    return my_env.state()
+
+def main():
+    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
